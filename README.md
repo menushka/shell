ba35bb9dd133e670ad4a39ca9b9f60e5ba5aed15
Menushka Weeratunga <contact@menushka.ca>
2024-10-05 19:15:08 -0400

    Update setup and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Shell
+
+## Install
+
+```bash
+TOKEN=<TOKEN> sh -c "$(curl -fsSL https://raw.githubusercontent.com/menushka/shell/refs/heads/master/setup.sh?token=$TOKEN)"
+```
--- setup.sh
+++ setup.sh
@@ -1,1 +1,64 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#!/bin/zsh
+
+# Check if Zsh is installed
+if ! command -v zsh &> /dev/null; then
+  echo "Zsh is not installed. Installing Zsh..."
+  
+  # Install Zsh on macOS or Linux
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    brew install zsh
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux (Debian-based)
+    sudo apt update && sudo apt install -y zsh
+  else
+    echo "Unsupported OS type. Exiting..."
+    exit 1
+  fi
+else
+  echo "Zsh is already installed."
+fi
+
+# Change the default shell to Zsh if it's not already the default
+if [ "$SHELL" != "$(which zsh)" ]; then
+  echo "Changing the default shell to Zsh..."
+  chsh -s "$(which zsh)"
+else
+  echo "Zsh is already the default shell."
+fi
+
+# Install Oh My Zsh if not installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh My Zsh is not installed. Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "Oh My Zsh is already installed."
+fi
+
+# Install Powerlevel10k theme if not installed
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+  echo "Powerlevel10k theme is not installed. Installing Powerlevel10k..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+else
+  echo "Powerlevel10k is already installed."
+fi
+
+# Set Zsh theme to Powerlevel10k in .zshrc
+if ! grep -q "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" "$HOME/.zshrc"; then
+  echo "Setting Powerlevel10k as the default theme in .zshrc..."
+  sed -i '' 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+else
+  echo "Powerlevel10k is already set as the default theme."
+fi
+
+# Download and copy the Powerlevel10k config file from the GitHub repo
+P10K_URL="https://raw.githubusercontent.com/menushka/shell/refs/heads/master/.p10k.zsh?token=$TOKEN"
+
+echo "Downloading Powerlevel10k config file from GitHub..."
+if curl -fsSL "$P10K_URL" -o "$HOME/.p10k.zsh"; then
+  echo "Powerlevel10k config file successfully downloaded and copied to home directory."
+else
+  echo "Failed to download Powerlevel10k config file. Please check the URL."
+fi
+
+echo "Setup completed!"
